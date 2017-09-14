0b36cc6918c26b73256a43e6e7a6037d7f34b23a
Renco Steenbergen <steenbergenrenco@gmail.com>
2017-09-14 18:53:00 +0200

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# coinmarketcap-scraper
-Write recent coin info data to a csv file every 5 minutes.
+# Coinmarketcap Scraper
+
+*Author* : [Renco Steenbergen](https://www.linkedin.com/in/renco-steenbergen-87b52a119/)
+
+*Technologies & Libraries* : Java SE 8, Slf4j
+
+*APIs* : [Coinmarketcap API](https://coinmarketcap.com/api/)
+
+*Summary* : Scraper tool to store cryptocurrency data with a Raspberry Pi
---
+++ src/main/java/com/rencomusic/coinmarketcap/scraper/Main.java
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//      2017-09-13 - Created by Renco Steenbergen                       //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+package com.rencomusic.coinmarketcap.scraper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Coinmarketcap scraper running.");
+    }
+}
